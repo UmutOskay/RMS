74a355af269091bf0b11a5571cb4187062af180d
Sefa Yapıcı <mustafasefayapici@gmail.com>
2021-12-13 19:47:04 +0300

    Opened a new Database and populated the database with example data

---
+++ ddl.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS room;
+DROP TABLE IF EXISTS reservations;
+
+
+
+CREATE TABLE IF NOT EXISTS user(
+	#user_index int auto_increment not null,
+    faculty_id VARCHAR(10) unique not null,
+    user_fullname MEDIUMTEXT not null,
+    user_mail VARCHAR(30)  unique not null,
+    user_password LONGTEXT not null,
+    is_admin BOOLEAN not null,
+    is_banned BOOLEAN not null,
+    time_slots_left int not null,
+    PRIMARY KEY (faculty_id)
+    );
+    
+CREATE TABLE IF NOT EXISTS reservations(
+	student_id MEDIUMTEXT not null,
+    room_id int not null,
+    time_slot MEDIUMTEXT not null
+);
+
+CREATE TABLE IF NOT EXISTS room(
+	room_id int not null,
+    room_location MEDIUMTEXT not null,
+    is_available_at_08 BOOLEAN not null,
+    is_available_at_09 BOOLEAN not null,
+    is_available_at_10 BOOLEAN not null,
+    is_available_at_11 BOOLEAN not null,
+    is_available_at_12 BOOLEAN not null,
+    is_available_at_13 BOOLEAN not null,
+    is_available_at_14 BOOLEAN not null,
+    is_available_at_15 BOOLEAN not null,
+    is_available_at_16 BOOLEAN not null,
+    is_available_at_17 BOOLEAN not null
+);
